e84fda218df6e3e95fadacee9c10021a82e340cf
Nikolai Nikitin <fliorgiz@gmail.com>
2025-12-04 18:33:45 +0700

    Set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/kameleoon_test_task_r_d/',
+    base: '/kameleoon_test_task_r_d/',
+    plugins: [react()],
 });
